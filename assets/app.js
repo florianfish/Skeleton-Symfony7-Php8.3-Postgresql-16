2c0a75679bfdcba81cdbe8d0989d3fb920ab87c6
Florian <florian@poissonmail.fr>
2024-10-28 12:30:59 +0000

    it works with demo symfo

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,24 @@
+// start the Stimulus application
+import './js/jquery_global.js';
+import $ from 'jquery';
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
+import 'highlight.js/styles/github-dark-dimmed.css';
+import 'lato-font/css/lato-font.css';
+import './js/scripts.js';
+
+// loads the Bootstrap plugins
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/scrollspy';
+
+// loads the code syntax highlighting library
+import './js/highlight.js';
+
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,10 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true]
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
